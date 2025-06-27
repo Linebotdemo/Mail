e7f7c64648fae628ffadb7496a8411c10ed0273a
Linebotdemo <k.inose0902@gmail.com>
2025-06-27 11:10:15 +0900

    fix: bind to correct port for Render

--- app.py
+++ app.py
@@ -2294,4 +2294,5 @@ if __name__ == '__main__':
         init_db()
     app.run(debug=True)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)), debug=True)
+
